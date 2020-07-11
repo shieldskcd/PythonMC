72d1fba9f0ca58a425ecec4fc4d1f54f8e4f7947
Dave Shields <66278753+shieldskcd@users.noreply.github.com>
2020-07-11 14:37:57 -0500

    Added Alex's code

---
+++ HelloWorld/Alex.py
@@ -0,0 +1,15 @@
+# This is Alex's first program
+
+#print("Hello I am Alex!")
+
+MyName=input("What is your name?")
+
+print("hello " + MyName)
+
+MyAge=input("How old are you?")
+
+print("Hello " + MyName + " you are " + MyAge + " years old!")
+
+MyFavFood = input("What is your favorite food?")
+
+print(MyName + " is my name! I am " + MyAge + " years old and I love " + MyFavFood)
